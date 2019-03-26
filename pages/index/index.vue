0bac3696940abda72365792e1d9679c2869db142
Augly <17633369350@163.com>
2019-03-26 18:02:52 +0800

    初始化代码

---
+++ pages/index/index.vue
@@ -0,0 +1,30 @@
+<template>
+	<view class="content"><view class="inputGroup"></view></view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			title: 'Hello'
+		};
+	},
+	onLoad() {},
+	methods: {}
+};
+</script>
+
+<style>
+.content {
+	text-align: center;
+	height: 400upx;
+}
+.inputGroup {
+	width: 704.16upx;
+	height: 59.72upx;
+	background: rgba(239, 239, 239, 1);
+	border-radius: 8.33upx;
+	margin:0 auto;
+	margin-top: 41.66upx;
+}
+</style>
