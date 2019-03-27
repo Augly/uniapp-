a8769c78985188416ffbc6fc7b3096cc2d448e8d
Augly <17633369350@163.com>
2019-03-27 18:03:47 +0800

    更新

--- App.vue
+++ App.vue
@@ -13,5 +13,6 @@
 </script>
 
 <style>
+	@import url("static/iconfont/iconfont.css");
 	/*每个页面公共css */
 </style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -7,21 +7,21 @@
 		</view>
 		<!-- 轮播图组件 -->
 		<view class="imageGroup">
-			<swiper class="swiper" previous-margin="9px" next-margin="9px" :indicator-dots="indicatorDots" :autoplay="autoplay"
-			 :interval="interval" :duration="duration" :indicator-color="indicatorColor" :indicator-active-color="activeColor"
-			 :circular="circular">
-				<!-- 				<block v-for="(item,index) in ">
-					
-				</block> -->
-				<swiper-item>
-					<view class="swiper-item uni-bg-red">A</view>
-				</swiper-item>
-				<swiper-item>
-					<view class="swiper-item uni-bg-green">B</view>
-				</swiper-item>
-				<swiper-item>
-					<view class="swiper-item uni-bg-blue">C</view>
-				</swiper-item>
+			<swiper
+				class="swiper"
+				previous-margin="9px"
+				next-margin="9px"
+				:indicator-dots="indicatorDots"
+				:autoplay="autoplay"
+				:interval="interval"
+				:duration="duration"
+				:indicator-color="indicatorColor"
+				:indicator-active-color="activeColor"
+				:circular="circular"
+			>
+				<swiper-item><view class="swiper-item uni-bg-red">A</view></swiper-item>
+				<swiper-item><view class="swiper-item uni-bg-green">B</view></swiper-item>
+				<swiper-item><view class="swiper-item uni-bg-blue">C</view></swiper-item>
 			</swiper>
 		</view>
 		<!-- 0元体验 -->
@@ -36,9 +36,7 @@
 					<span>洗脸仪</span>
 				</view>
 			</view>
-			<view class="zeroConter">
-
-			</view>
+			<view class="zeroConter"></view>
 			<view class="zeroMintitle">
 				<block>
 					<image src="../../static/shuliang@2x.png" mode="" class="icon"></image>
@@ -56,165 +54,374 @@
 		</view>
 		<!-- 邀请好友 -->
 		<view class="shareFriends">
-			
+			<view class="left">
+				<span class="title">邀请有礼</span>
+				<span class="mintitle">邀请好友赢现金</span>
+			</view>
+			<view class="right">
+				<span class="title">邀请</span>
+				<image src="../../static/返回%20拷贝%203@2x.png" mode="" class="img"></image>
+			</view>
+		</view>
+		<!--listOne-->
+		<view class="listOne">
+			<view class="listTitle">
+				<span class="title">酷玩娱乐</span>
+				<span class="more">查看更多</span>
+				<image src="../../static/返回%20拷贝%203@2x.png" mode=""></image>
+			</view>
+			<view class="listContent">
+				<scroll-view class="scrollWrap" scroll-x="true">
+					<view class="listWrap">
+						<view class="imgWrap"></view>
+						<view class="listRes">
+							<span class="title">Bose蓝牙耳机</span>
+							<span class="price">￥20.00/天</span>
+						</view>
+					</view>
+					<view class="listWrap">
+						<view class="imgWrap"></view>
+						<view class="listRes">
+							<span class="title">Bose蓝牙耳机</span>
+							<span class="price">￥20.00/天</span>
+						</view>
+					</view>
+					<view class="listWrap">
+						<view class="imgWrap"></view>
+						<view class="listRes">
+							<span class="title">Bose蓝牙耳机</span>
+							<span class="price">￥20.00/天</span>
+						</view>
+					</view>
+					<view class="listWrap">
+						<view class="imgWrap"></view>
+						<view class="listRes">
+							<span class="title">Bose蓝牙耳机</span>
+							<span class="price">￥20.00/天</span>
+						</view>
+					</view>
+				</scroll-view>
+			</view>
+		</view>
+		<view class="listOne">
+			<view class="listTitle">
+				<span class="title">酷玩娱乐</span>
+				<span class="more">查看更多</span>
+				<image src="../../static/返回%20拷贝%203@2x.png" mode=""></image>
+			</view>
+			<view class="listContent">
+				<scroll-view class="scrollWrap" scroll-x="true">
+					<view class="listWrap">
+						<view class="imgWrap"></view>
+						<view class="listRes">
+							<span class="title">Bose蓝牙耳机</span>
+							<span class="price">￥20.00/天</span>
+						</view>
+					</view>
+					<view class="listWrap">
+						<view class="imgWrap"></view>
+						<view class="listRes">
+							<span class="title">Bose蓝牙耳机</span>
+							<span class="price">￥20.00/天</span>
+						</view>
+					</view>
+					<view class="listWrap">
+						<view class="imgWrap"></view>
+						<view class="listRes">
+							<span class="title">Bose蓝牙耳机</span>
+							<span class="price">￥20.00/天</span>
+						</view>
+					</view>
+					<view class="listWrap">
+						<view class="imgWrap"></view>
+						<view class="listRes">
+							<span class="title">Bose蓝牙耳机</span>
+							<span class="price">￥20.00/天</span>
+						</view>
+					</view>
+				</scroll-view>
+			</view>
 		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				indicatorDots: true,
-				autoplay: true,
-				interval: 2000,
-				duration: 2000,
-				indicatorColor: 'rgba(255,255,255,0.6)',
-				activeColor: 'rgba(255,255,255,1)',
-				circular: true
-			};
-		},
-		onLoad() {},
-		methods: {
-			handleChange() {
-
-			},
-		}
-	};
+export default {
+	data() {
+		return {
+			indicatorDots: true,
+			autoplay: true,
+			interval: 2000,
+			duration: 500,
+			indicatorColor: 'rgba(255,255,255,0.6)',
+			activeColor: 'rgba(255,255,255,1)',
+			circular: true
+		};
+	},
+	onLoad() {},
+	methods: {
+		handleChange() {}
+	}
+};
 </script>
 
 <style lang="less">
-	.content {
-		text-align: center;
-		height: 400upx;
+.content {
+	position: relative;
+	width: 100%;
+	height: 100%;
+	overflow: hidden;
+	background-color: rgba(255, 255, 255, 1);
+}
+::-webkit-scrollbar {
+	display: none;
+}
+// 搜索组件样式
+.inputGroup {
+	width: 704.16upx;
+	height: 59.72upx;
+	background: rgba(239, 239, 239, 1);
+	border-radius: 8.33upx;
+	margin: 0 auto;
+	margin-top: 41.66upx;
+	box-sizing: border-box;
+	padding: 0upx 15.27upx;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+
+	image {
+		width: 26.38upx;
+		height: 26.38upx;
 	}
 
-	// 搜索组件样式
-	.inputGroup {
-		width: 704.16upx;
-		height: 59.72upx;
-		background: rgba(239, 239, 239, 1);
-		border-radius: 8.33upx;
-		margin: 0 auto;
-		margin-top: 41.66upx;
-		box-sizing: border-box;
-		padding: 0upx 15.27upx;
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-
-		image {
-			width: 26.38upx;
-			height: 26.38upx
-		}
-
-		input {
-			width: 625upx;
-			font-size: 27.77upx;
-			color: #989898;
-			text-align: left;
-		}
+	input {
+		width: 625upx;
+		font-size: 27.77upx;
+		color: #989898;
+		text-align: left;
 	}
+}
 
-	// 3D轮播样式
-	.imageGroup {
+// 3D轮播样式
+.imageGroup {
+	width: 100%;
+	height: 322.22upx;
+	margin: 41.66upx 0upx;
+
+	.swiper {
 		width: 100%;
-		height: 322.22upx;
-		margin: 41.66upx 0upx;
+		height: 100%;
 
-		.swiper {
+		.swiper-item {
+			border-radius: 9.72upx;
 			width: 100%;
 			height: 100%;
+		}
+	}
+}
 
-			.swiper-item {
-				border-radius: 9.72upx;
-				width: 100%;
-				height: 100%
+// 0元体验
+.zeroExperience {
+	width: 100%;
+	height: auto;
+	box-sizing: border-box;
+	padding: 0upx 22.22upx;
+
+	.zero-title {
+		width: 100%;
+		display: flex;
+		align-items: center;
+
+		.left {
+			display: flex;
+			align-items: center;
+			width: 50%;
+
+			image {
+				width: 30.55upx;
+				height: 30.55upx;
+			}
+
+			span {
+				margin-left: 16.66upx;
+			}
+
+			font-size: 34.72upx;
+			justify-content: flex-start;
+		}
+
+		.right {
+			width: 50%;
+			display: flex;
+			align-items: center;
+			justify-content: flex-end;
+			font-size: 27.77upx;
+
+			span {
+				margin-left: 66.66upx;
+			}
+
+			.active {
+				color: rgba(45, 171, 247, 1);
 			}
 		}
 	}
 
-	// 0元体验
-	.zeroExperience {
+	.zeroConter {
+		width: 100%;
+		height: 280.55upx;
+		border-radius: 9.72upx;
+		background-color: aliceblue;
+		margin: 27.77upx 0upx;
+	}
+	.zeroMintitle {
+		width: 100%;
+		display: flex;
+		align-items: center;
+		.icon {
+			width: 25upx;
+			height: 25upx;
+		}
+		span {
+			font-size: 25upx;
+			color: rgba(102, 102, 102, 1);
+			margin-left: 11.11upx;
+			margin-right: 61.11upx;
+		}
+	}
+}
+// 邀请有礼物
+.shareFriends {
+	width: 705.55upx;
+	height: 141.66upx;
+	margin: 0 auto;
+	margin-top: 27.77upx;
+	margin-bottom: 27.77upx;
+	background-color: #ffffff;
+	border-radius: 13.88upx;
+	box-shadow: 0upx 0upx 27.77upx #f1f1f1;
+	box-sizing: border-box;
+	padding: 30.55upx 27.77upx;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	.left {
+		width: 50%;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		align-items: flex-start;
+		.title {
+			font-size: 23.61upx;
+			font-weight: 400;
+			color: rgba(21, 21, 21, 1);
+		}
+		.mintitle {
+			font-size: 20.83upx;
+			font-weight: 400;
+			color: rgba(181, 181, 181, 1);
+		}
+	}
+	.right {
+		height: 100%;
+		width: 50%;
+		display: flex;
+		align-items: center;
+		justify-content: flex-end;
+		.title {
+			font-size: 26.38upx;
+			font-weight: 400;
+			color: rgba(237, 217, 166, 1);
+		}
+		.img {
+			width: 12.5upx;
+			height: 22.22upx;
+			margin-left: 27.77upx;
+		}
+	}
+}
+
+//列表组件1
+.listOne {
+	width: 100%;
+	box-sizing: border-box;
+	padding: 0upx 0upx 0upx 22.22upx;
+	.listTitle {
 		width: 100%;
 		height: auto;
 		box-sizing: border-box;
-		padding: 0upx 22.22upx;
-
-		.zero-title {
+		padding: 0upx 22.22upx 0upx 0upx;
+		display: flex;
+		align-items: center;
+		margin: 27.77upx 0upx;
+		justify-items: flex-end;
+		.title {
+			font-size: 34.72upx;
+			font-family: PingFangSC-Regular;
+			font-weight: 400;
+			color: rgba(51, 51, 51, 1);
+			margin-right: auto;
+		}
+		.more {
+			font-size: 25upx;
+			font-family: PingFangSC-Regular;
+			font-weight: 400;
+			color: rgba(153, 153, 153, 1);
+		}
+		image {
+			width: 13.88upx;
+			height: 25upx;
+			margin-left: 13.88upx;
+		}
+	}
+	.listContent {
+		width: 100%;
+		height: auto;
+		.scrollWrap {
+			white-space: nowrap;
 			width: 100%;
-			display: flex;
-			align-items: center;
-
-			.left {
+		}
+		.listWrap {
+			display: inline-block;
+			white-space: nowrap;
+			width: 204.16upx;
+			height: 312.5upx;
+			margin-right: 15.27upx;
+			.imgWrap {
+				width: 100%;
+				height: 204.16upx;
+				background-color: red;
+			}
+			.listRes {
+				width: 100%;
+				height: 83.33upx;
+				margin-top: 25upx;
 				display: flex;
-				align-items: center;
-				width: 50%;
+				flex-direction: column;
+				justify-content: space-between;
 
-				image {
-					width: 30.55upx;
-					height: 30.55upx
+				.title {
+					font-size: 31.94upx;
+					font-weight: 400;
+					color: rgba(0, 0, 0, 1);
 				}
-
-				span {
-					margin-left: 16.66upx
-				}
-
-				font-size: 34.72upx;
-				justify-content: flex-start;
-			}
-
-			.right {
-				width: 50%;
-				display: flex;
-				align-items: center;
-				justify-content: flex-end;
-				font-size: 27.77upx;
-
-				span {
-					margin-left: 66.66upx;
-
-				}
-
-				.active {
-					color: rgba(45, 171, 247, 1)
+				.price {
+					font-size: 25upx;
+					font-weight: 400;
+					color: rgba(45, 171, 247, 1);
 				}
 			}
 		}
+	}
+}
 
-		.zeroConter {
-			width: 100%;
-			height: 280.55upx;
-			border-radius: 9.72upx;
-			background-color: aliceblue;
-			margin: 27.77upx 0upx;
-		}
-		.zeroMintitle{
-			width: 100%;
-			display: flex;
-			align-items: center;
-			.icon{
-				width: 25upx;
-				height: 25upx;
-			}
-			span{
-				font-size: 25upx;
-				color: rgba(102, 102, 102, 1);
-				margin-left: 11.11upx;
-				margin-right: 61.11upx;
-			}
-		}
-	}
-	.shareFriends{
-		width: 730.55upx;
-		height: 141.66upx;
-		margin: 0 auto;
-	}
-	.uni-bg-red {
-		background-color: red;
-	}
+.uni-bg-red {
+	background-color: red;
+}
 
-	.uni-bg-green {
-		background-color: green;
-	}
+.uni-bg-green {
+	background-color: green;
+}
 </style>
